6d23a1392bcf4eaa85a6bbadbc8a436a4bc862ae
Marco Pacheco <marcoa829@gmail.com>
2024-05-17 16:33:54 -0300

    Update and rename mover.cs to Mover.cs

--- obstacleGame/Mover.cs
+++ obstacleGame/Mover.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mover : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to my game!");
+        Debug.Log("Please move using W A S D");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
         transform.Translate(xValue,0,zValue);
     }
 }
