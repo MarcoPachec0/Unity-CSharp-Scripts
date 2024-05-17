092f1fde152ffc215a0f337023ccfa598e6ec33a
Marco Pacheco <marcoa829@gmail.com>
2024-05-17 16:32:43 -0300

    Add files via upload

---
+++ obstacleGame/Scorer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+    private void OnCollisionEnter(Collision other) 
+    {
+        hits = hits + 1;
+        Debug.Log("You hit me " + hits + " times!");
+    }
+}
