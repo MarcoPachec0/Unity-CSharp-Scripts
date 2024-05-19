bddfec871a9e1809e91b4aa6b000d568d7d2d3f5
Marco Pacheco <marcoa829@gmail.com>
2024-05-19 16:09:15 -0300

    Update Scorer.cs

--- obstacleGame/Scorer.cs
+++ obstacleGame/Scorer.cs
@@ -7,7 +7,10 @@ public class Scorer : MonoBehaviour
     int hits = 0;
     private void OnCollisionEnter(Collision other) 
     {
-        hits = hits + 1;
-        Debug.Log("You hit me " + hits + " times!");
+        if(other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You hit me " + hits + " times!");
+        }
     }
 }
